dcad1698c78f08e2815f3859a3d18744b2e760ea
aacha0 <148589444+aacha0@users.noreply.github.com>
2023-11-30 22:47:46 -0800

    Update bonus.sql

--- 8 Weeks SQL Challenges /case study 5: data mart/bonus.sql
+++ 8 Weeks SQL Challenges /case study 5: data mart/bonus.sql
@@ -1,3 +1,28 @@
+DROP TABLE IF EXISTS before_after;
+    Create table before_after(
+    select *, 
+    case when week_date < '2020-06-15' then 'before' else 'after' end as new_packaging 
+    from clean_weekly_sales);
+    
+DROP TABLE IF EXISTS comparison;
+Create table comparison(
+  with cte as(
+select *, dense_rank()over(order by week_number desc) ranking 
+from before_after  
+where new_packaging = 'before' 
+union
+select *, dense_rank()over(order by week_number) ranking 
+from before_after  
+where new_packaging = 'after')
+select week_date, ranking, region, platform, age_band, demographic, customer_type,new_packaging, 
+sum(sales)over(partition by region,new_packaging)region_sum, sum(sales)over(partition by platform,new_packaging)platform_sum,
+sum(sales)over(partition by age_band,new_packaging)age_band_sum,
+sum(sales)over(partition by demographic,new_packaging)demographic_sum, sum(sales)over(partition by customer_type,new_packaging)customer_type_sum
+  from cte
+  where ranking between 1 and 12);
+
+
+
 
 #region differences 
  select  distinct a.region, (b.region_sum  - a.region_sum) difference, round((b.region_sum  - a.region_sum)/a.region_sum *100,2) percentage_diff
